ae1d5e4174cdaf66f115b8c9e5f72b84113f2c93
qqq <qqq>
2022-11-24 17:18:55 +0800

    Initial commit

---
+++ dataset.py
@@ -0,0 +1,154 @@
+import torch
+import numpy as np
+from faker import Faker
+import random
+from babel.dates import format_date
+import matplotlib.pyplot as plt
+from functools import partial
+from tqdm import tqdm
+
+FORMATS = ['short',
+           'medium',
+           'long',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'full',
+           'd MMM YYY', 
+           'd MMMM YYY',
+           'dd MMM YYY',
+           'd MMM, YYY',
+           'd MMMM, YYY',
+           'dd, MMM YYY',
+           'd MM YY',
+           'd MMMM YYY',
+           'MMMM d YYY',
+           'MMMM d, YYY',
+           'dd.MM.YY']
+
+def transform(human_readable, machine_readable, human_vocab, machine_vocab):
+    X = list(map(lambda x: human_vocab.get(x, '<unk>'), human_readable))
+    Y = list(map(lambda x: machine_vocab.get(x, '<unk>'), machine_readable)) #len(Y) is always 10, because the format is YYYY-MM-DD
+    '''
+    我们输入当网络中的是[machine_vocab['<pad>']] + Y
+    我们输出的目标是Y + [machine_vocab['<pad>']]
+    因为第一次解码只使用了的最开始的<pad>（由mask控制），此时解码出来的字符应该是Y[0]
+    最后一次解码时，使用了全部[machine_vocab['<pad>']] + Y，则目标输出就是Y + [machine_vocab['<pad>']]
+    '''
+    Y = [machine_vocab['<pad>']] + Y + [machine_vocab['<pad>']]
+    # print(human_readable)
+    # print(machine_readable)
+    # print(X)
+    # print(Y)
+    def zcs(length, idx):
+        ret = np.zeros(length)
+        ret[idx] = 1
+        return ret
+    # Xoh = np.array(list(map(lambda x: to_categorical(x, num_classes=len(human_vocab)), X)))
+    Xoh = np.array(list(map(partial(zcs, len(human_vocab)), X)), dtype=np.float32)
+    Yoh = np.array(list(map(partial(zcs, len(machine_vocab)), Y)), dtype=np.float32) #如果使用交叉熵loss，pytorch不需要label是onehot的；通过对比发现用MSELoss更好一些
+    return Xoh, Yoh, {'human_readable':human_readable, 'machine_readable':machine_readable}
+    # return Xoh, np.array(Y)
+
+def collate_fn(pad_vec, batch):
+    '''
+    对于短的输入，onehost向量较少，需要补充<pad>对应的onehot向量，即pad_vec
+    '''
+    pad_vec = pad_vec.reshape(-1, 1)
+    batch_x, batch_y, extra = zip(*batch)
+    # print(batch_x.shape, batch_y.shape)
+    max_len_x = max(x.shape[0] for x in batch_x)
+
+    batch_x_paded = []
+    for x in batch_x:
+        pad_len = max_len_x - x.shape[0]
+        pad = np.repeat(pad_vec, pad_len, axis=1).T
+        batch_x_paded.append(np.vstack((x, pad)))
+        # print(x.shape)
+
+    batch_x = torch.FloatTensor(batch_x_paded)
+    batch_y = torch.FloatTensor(batch_y)
+    return batch_x, batch_y, extra
+    # print(max_len_x, max_len_y)
+    
+
+# def collate_fn(batch):
+#     batch_x, batch_y = zip(*batch)
+#     batch_x = [torch.FloatTensor(x) for x in batch_x]
+#     batch_y = torch.nn.utils.rnn.pad_sequence(batch_x, batch_first=True)
+#     return batch_x, batch_y
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, transform, n_datas=10000, seed=12345):
+        self.transform = transform
+
+        self.fake = Faker()
+        if seed is not None:
+            Faker.seed(seed)
+            random.seed(seed)
+
+        self.human_vocab = set()
+        self.machine_vocab = set()
+        self.dataset = []
+        for i in tqdm(range(n_datas)):
+            human_readable, machine_readable = self.load_date()
+            self.dataset.append((human_readable, machine_readable))
+            self.human_vocab.update(tuple(human_readable))
+            self.machine_vocab.update(tuple(machine_readable))
+
+        self.human_vocab = dict(zip(sorted(self.human_vocab) + ['<unk>', '<pad>'], list(range(len(self.human_vocab) + 2))))
+        '''
+        使用transformer时，需要给target序列也传给decoder，但是需要加mask，因为解码第4个输出时，只能使用前面0123这四个特征；后文有详解
+        由于加mask后要进行softmax，因此至少得有一个有效元素
+        但是解码第一个元素时，我们可供使用的target序列长度是0
+        因此必须在最前边加一个<pad>
+        '''
+        self.inv_machine_vocab = dict(enumerate(sorted(self.machine_vocab) + ['<pad>']))
+        self.machine_vocab = {v:k for k, v in self.inv_machine_vocab.items()}
+        # print(self.human_vocab)
+        # print(self.machine_vocab)
+        # print(self.inv_machine_vocab)
+
+    def __getitem__(self, idx):
+        human_readable, machine_readable = self.dataset[idx] #dataset is a list of tuples
+        return self.transform(human_readable, machine_readable, self.human_vocab, self.machine_vocab)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def load_date(self):
+        """
+            Loads some fake dates 
+            :returns: tuple containing human readable string, machine readable string, and date object
+        """
+        dt = self.fake.date_object()
+
+        human_readable = format_date(dt, format=random.choice(FORMATS),  locale='en_US')
+        human_readable = human_readable.lower()
+        human_readable = human_readable.replace(',', '')
+
+        machine_readable = dt.isoformat()
+        return human_readable, machine_readable
+
+if __name__ == '__main__':
+    dataset = Dataset(transform=transform, n_datas=1000)
+    pad_vec = np.zeros(len(dataset.human_vocab))
+    pad_vec[dataset.human_vocab['<pad>']] = 1
+    dataloader = torch.utils.data.DataLoader(dataset=dataset,
+                                            batch_size=4,
+                                            shuffle=False,
+                                            num_workers=0,
+                                            collate_fn=partial(collate_fn, pad_vec))
+
+    for step, (batch_x, batch_y, extra) in enumerate(dataloader):
+        print(step, batch_x.shape, batch_y.shape)
+        # print(batch_x[0], extra[0]['human_readable'])
+        print(batch_y[0], extra[0]['machine_readable']) #extra中的machine_readable部分长度是10，onehot向量是11个，其中第0个对应的是<pad>
+        if step >= 0:
+            break
---
+++ test.py
@@ -0,0 +1,94 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dataset import Dataset, transform
+from model import Transformer
+from util import save_load as sl
+
+def translate(model, x, y0): #这里只支持batch_size=1
+    x_tensor = torch.from_numpy(x).unsqueeze(0) #(1, T, 37)， 37是输入数据词汇表长度
+    enc_output = model.encoder(x_tensor) #编码部分可以先获取到
+    y0_tensor = torch.from_numpy(y0).unsqueeze(0).unsqueeze(0) #(1, 1, 12)， 12是输出数据词汇表长度，解码最开始时只有开头的<pad>是已知的
+    y_tensor = y0_tensor.clone()
+    
+    for step in range(14): #解码14次足以
+        dec_output = model.decoder(enc_output, y_tensor) #以下三行是transformer的解码部分以及后面的模块
+        pred = model.linear(dec_output)
+        pred = torch.nn.Softmax(dim=-1)(pred) #this is very impotent
+
+        y_tensor = torch.cat((y0_tensor, pred), dim=1) #解码得到的pred是除去开头<pad>字符的序列，下一次解码仍然需要<pad>
+        # print(y_tensor.shape)
+    return y_tensor.squeeze(0) #去除batch size
+
+def paint_score(score, human_readable, pred):
+    '''
+    score: (n_head, T_dec, T_enc)
+    human_readabe: string, length is T_enc
+    pred: string, length is T_dec
+    '''
+    # print(score.shape, len(pred), len(human_readable))
+    # print(pred, human_readable)
+
+    n_head = score.shape[0]
+
+    f = plt.figure(figsize=(8, 8.5))
+    for i in range(n_head):
+        ax = f.add_subplot(n_head, 1, i+1)
+        i = ax.imshow(score[i, :10, :], interpolation='nearest', cmap='Blues')
+
+        ax.set_xticks(range(30))
+        ax.set_xticklabels(human_readable[:30], rotation=0)
+
+        ax.set_yticks(range(10))
+        ax.set_yticklabels(pred[:10], rotation=0)
+
+    plt.savefig('./attention.png')
+
+def main():
+    dataset = Dataset(transform=transform, n_datas=10000, seed=None) #生成10000个数据，确保字符都出现
+    model = Transformer(n_head=2)
+    try:
+        trained_epoch = sl.find_last_checkpoint('./checkpoint')
+        print('load model %d' % (trained_epoch))
+    except Exception as e:
+        print('no trained model found, {}'.format(e))
+        return
+    model = sl.load_model('./checkpoint', -1, model)
+    model.eval()
+
+    x, y, extra = dataset.__getitem__(0) #值使用y的第0个特征向量，即<pad>对应的onehot向量
+    # print(x.shape, y.shape)
+    # pred = model(torch.from_numpy(x).unsqueeze(0), torch.from_numpy(y).unsqueeze(0)).squeeze()
+    pred = translate(model, x, y[0]) #日期格式转换时，对于输入序列，我们全部知道；但是对于输出序列，只有开头的<pad>的已知的
+    # print(pred.shape)
+    pred = np.argmax(pred.detach().numpy(), axis=1)[1:]
+    # print(extra['machine_readable'])
+    pred = [dataset.inv_machine_vocab[p] for p in pred]
+    pred_str = ''.join(pred)
+    human_readable = extra['human_readable']
+    machine_readable = extra['machine_readable']
+    print('[%s] --> [%s], answer: [%s]' % (human_readable, pred_str, list(machine_readable)))
+
+    dec_scores = model.decoder.scores_for_paint
+    # print(dec_scores.shape)
+    paint_score(dec_scores[0], human_readable, pred)#[0]是去batch中的第0个
+
+    # print(len(model.scores_for_paint), model.scores_for_paint[0].shape)
+    # scores = np.array(model.scores_for_paint)
+    # print(np.argmax(scores, axis=1))
+
+    # f = plt.figure(figsize=(8, 8.5))
+    # ax = f.add_subplot(1, 1, 1)
+    # i = ax.imshow(scores, interpolation='nearest', cmap='Blues')
+
+    # ax.set_xticks(range(30))
+    # ax.set_xticklabels(human_readable[:30], rotation=0)
+
+    # ax.set_yticks(range(10))
+    # ax.set_yticklabels(machine_readable[:10], rotation=0)
+
+    # plt.savefig('./attention.png')
+
+if __name__ == '__main__':
+    main()
